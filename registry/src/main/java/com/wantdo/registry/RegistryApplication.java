c55b42a297afd989ab69f513d5e31165bd14b66b
luanx <luanx@wantdo.com>
2017-11-13 18:25:00 +0800

    add module registry

---
+++ registry/src/main/java/com/wantdo/registry/RegistryApplication.java
@@ -0,0 +1,19 @@
+package com.wantdo.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: luanx
+ * @email: luanx@wantdo.com
+ * @date: 2017-11-13
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+}

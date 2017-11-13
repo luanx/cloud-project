7e31daa99795aa93400f185774e53c07d430264f
luanx <luanx@wantdo.com>
2017-11-13 18:31:26 +0800

    add module config

---
+++ config/src/main/java/com/wantdo/config/ConfigApplication.java
@@ -0,0 +1,21 @@
+package com.wantdo.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author: luanx
+ * @email: luanx@wantdo.com
+ * @date: 2017-11-13
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}

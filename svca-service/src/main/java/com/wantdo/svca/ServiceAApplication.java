546b2554eaa0efcd0040877d723a8e4123262b39
luanx <luanx@wantdo.com>
2017-11-13 18:43:55 +0800

    add module svca-service and svcb-service

---
+++ svca-service/src/main/java/com/wantdo/svca/ServiceAApplication.java
@@ -0,0 +1,14 @@
+package com.wantdo.svca;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: luanx
+ * @email: luanx@wantdo.com
+ * @date: 2017-11-13
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServiceAApplication {
+}
---
+++ svcb-service/src/main/java/com/wantdo/svcb/ServiceBApplication.java
@@ -0,0 +1,15 @@
+package com.wantdo.svcb;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: luanx
+ * @email: luanx@wantdo.com
+ * @date: 2017-11-13
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServiceBApplication {
+}

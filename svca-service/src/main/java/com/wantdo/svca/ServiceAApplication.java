e0194e56f699ff729334e72594f211a64e08a1c3
luanx <luanx@wantdo.com>
2017-11-14 10:43:40 +0800

    add Main class for svca and svcb

--- svca-service/src/main/java/com/wantdo/svca/ServiceAApplication.java
+++ svca-service/src/main/java/com/wantdo/svca/ServiceAApplication.java
@@ -1,5 +1,6 @@
 package com.wantdo.svca;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
@@ -11,4 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ServiceAApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAApplication.class, args);
+    }
 }
--- svcb-service/src/main/java/com/wantdo/svcb/ServiceBApplication.java
+++ svcb-service/src/main/java/com/wantdo/svcb/ServiceBApplication.java
@@ -1,5 +1,6 @@
 package com.wantdo.svcb;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
@@ -12,4 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ServiceBApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceBApplication.class, args);
+    }
 }
